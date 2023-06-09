9c04e5a0900cb9d813bda80e439b43f99d337909
Arifia Kasastra R <arka.progammer@gmail.com>
2023-06-09 16:51:36 +0700

    WIP

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -58,7 +58,7 @@
                         </svg>
                         <span class="ml-3">Customer Service</span>
                     </a>
-                    <a href="/access-log?file=b3835394-access.log"
+                    <a href="/access-log"
                         class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 group transition duration-75 flex items-center p-2">
                         <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
                             fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
