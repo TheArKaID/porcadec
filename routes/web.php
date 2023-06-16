abcb9c833f1a1a6583f901c22dc0c23e74fafeb9
Arifia Kasastra R <arka.progammer@gmail.com>
2023-06-16 12:41:23 +0700

    Fix Dashboard Auth

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,10 @@ Route::get('/', function () {
     return redirect('/dashboard');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
